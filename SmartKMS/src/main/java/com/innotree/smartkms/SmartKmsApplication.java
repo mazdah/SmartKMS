428c2ae618b12f7466dcc35cff841a2c342268ad
WOO HYOUNG JUN <woohj70@gmail.com>
2018-03-03 22:54:37 +0900

    initial commit

---
+++ SmartKMS/src/main/java/com/innotree/smartkms/SmartKmsApplication.java
@@ -0,0 +1,12 @@
+package com.innotree.smartkms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartKmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartKmsApplication.class, args);
+	}
+}
---
+++ SmartKMS/src/test/java/com/innotree/smartkms/SmartKmsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.innotree.smartkms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmartKmsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
